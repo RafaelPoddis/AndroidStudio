f7d5d0ae65d1e1252d2ce830376d45bfaa4bbe06
RafaelPoddis <rafael.poddis@gmail.com>
2026-09-09 12:36:11 -0300

    Lógica do botao de menu na tela de IMC

--- app/src/main/java/com/example/aula01/IMC.kt
+++ app/src/main/java/com/example/aula01/IMC.kt
@@ -3,6 +3,7 @@ package com.example.aula01
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
@@ -13,6 +14,12 @@ class IMC : ComponentActivity() {
         enableEdgeToEdge()
 
         setContentView(R.layout.activity_imc)
+
+        val menuBtn = findViewById<ImageButton>(R.id.menuBtn)
+
+        menuBtn.setOnClickListener {
+            startActivity(Intent(this, Menu::class.java))
+        }
 
         val altura = findViewById<EditText>(R.id.alturaValue).text.toString().toDoubleOrNull()
         val peso = findViewById<EditText>(R.id.pesoValue).text.toString().toDoubleOrNull()
