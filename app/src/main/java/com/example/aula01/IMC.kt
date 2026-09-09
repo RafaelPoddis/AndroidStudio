f6351f8feb434fb8d4afcc9b2404eee8ab7faf75
RafaelPoddis <rafael.poddis@gmail.com>
2026-09-09 12:50:09 -0300

    ajustes na logica do IMC

--- app/src/main/java/com/example/aula01/IMC.kt
+++ app/src/main/java/com/example/aula01/IMC.kt
@@ -22,25 +22,27 @@ class IMC : ComponentActivity() {
             startActivity(Intent(this, Menu::class.java))
         }
 
-        val altura = findViewById<EditText>(R.id.alturaValue).text.toString().toDoubleOrNull()
-        val peso = findViewById<EditText>(R.id.pesoValue).text.toString().toDoubleOrNull()
+        val alturaView = findViewById<EditText>(R.id.alturaValue)
+        val pesoView = findViewById<EditText>(R.id.pesoValue)
         val resultLabel = findViewById<TextView>(R.id.resultadoImc)
 
         val calculateBtn = findViewById<Button>(R.id.calculateImc)
 
         calculateBtn.setOnClickListener {
+            val altura = alturaView.text.toString().toDoubleOrNull()
+            val peso = pesoView.text.toString().toDoubleOrNull()
             if (altura == null || peso == null){
                 resultLabel.text = getText(R.string.invalidValue)
                 return@setOnClickListener
             }
             val imc = calculateIMC(altura=altura, peso=peso)
             when {
-                imc < 18.5 -> resultLabel.text = "Seu IMC é de $imc - Abaixo do peso"
-                imc < 25.0 -> resultLabel.text = "Seu IMC é de $imc - Peso normal"
-                imc < 30.0 -> resultLabel.text = "Seu IMC é de $imc - Sobrepeso"
-                imc < 35.0 -> resultLabel.text = "Seu IMC é de $imc - Obesidade grau I"
-                imc < 40.0 -> resultLabel.text = "Seu IMC é de $imc - Obesidade grau II"
-                else -> resultLabel.text = "Seu IMC é de $imc - Obesidade grau III"
+                imc < 18.5 -> resultLabel.text = "Seu IMC é de %.2f - Abaixo do peso".format(imc)
+                imc < 25.0 -> resultLabel.text = "Seu IMC é de %.2f - Peso normal".format(imc)
+                imc < 30.0 -> resultLabel.text = "Seu IMC é de %.2f - Sobrepeso".format(imc)
+                imc < 35.0 -> resultLabel.text = "Seu IMC é de %.2f - Obesidade grau I".format(imc)
+                imc < 40.0 -> resultLabel.text = "Seu IMC é de %.2f - Obesidade grau II".format(imc)
+                else -> resultLabel.text = "Seu IMC é de %.2f - Obesidade grau III".format(imc)
             }
         }
     }
